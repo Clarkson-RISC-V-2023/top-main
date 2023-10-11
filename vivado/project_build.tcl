710636a4f6a475fc60e93425330c5a9dafc5d6a7
Ernesto Sola-Thomas <ernesto@solathomas.com>
2023-10-11 15:30:06 -0400

    Update .tcl

--- vivado/project_build.tcl
+++ vivado/project_build.tcl
@@ -16,12 +16,11 @@ add_files -fileset constrs_1 -norecurse $PROJECT_CONSTRAINT_FILE
 
 source ./vivado/read_verilog_rtl.tcl
 
-update_compile_order -fileset RISCy_RTL
+set_property top top [current_fileset]
+update_compile_order -fileset sources_1
 
 # Launch Synthesis and wait on completion
-launch_runs RISCy_synth_1
-wait_on_run RISCy_synth_1
-open_run RISCy_synth_1 -name RISCy_netlist_1
+synth_design -top top -part $PART_NUM -lint
 
 # Generate a timing and power reports and write to disk
 report_timing_summary -delay_type max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins -file ${DIR_OUTPUT}/syn_timing.rpt
