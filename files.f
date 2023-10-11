a006324a2d4e36a4998356167c295c0f8b871027
Ernesto Sola-Thomas <ernesto@solathomas.com>
2023-10-10 21:56:30 -0400

    Add file list for all repos

---
+++ files.f
@@ -0,0 +1,24 @@
+-f ./src/ip/alu/files.f
+
+./src/ip/branch/rtl/branch.sv
+./src/ip/branch/verif/tb_branch.sv
+
+./src/ip/decoder/rtl/decoder.sv
+./src/ip/decoder/rtl/riscv_instr.sv
+./src/ip/decoder/verif/tb_decoder.sv
+
+./src/ip/jump/rtl/jump.sv
+./src/ip/jump/verif/tb_jump.sv
+
+-f ./src/ip/mem/files.f
+
+./src/ip/lsu/rtl/lsu.sv
+./src/ip/lsu/verif/tb_lsu.sv
+
+./src/ip/pc/rtl/pc.sv
+./src/ip/pc/verif/tb_pc.sv
+
+-f ./src/ip/regs/files.f
+
+./src/ip/sextend/rtl/sign_extend.sv
+./src/ip/sextend/verif/tb_sign_extend.sv
