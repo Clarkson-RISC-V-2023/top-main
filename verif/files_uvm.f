1fa819746fc74e77691908751783b48aebb0a0b2
Ernesto Sola-Thomas <ernesto@solathomas.com>
2023-10-15 14:22:06 -0400

    Initial File Structure

---
+++ verif/files_uvm.f
@@ -0,0 +1,18 @@
+-f ../files.f
+
+./Interface/single_cycle_vif.svh
+
+./Sequencer/Sequence_items/*
+./Sequencer/single_cycle_sequencer.svh
+
+./Driver/single_cycle_driver.svh
+
+./Scoreboard/single_cycle_scoreboard.svh
+
+./Monitor/single_cycle_monitor.svh
+
+./Agent/sngle_cycle_agent.svh
+
+./Env/single_cycle_env.svh
+
+./Test/single_cycle_test_1.svh
