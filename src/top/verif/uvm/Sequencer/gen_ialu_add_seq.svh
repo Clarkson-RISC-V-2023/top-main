c8ed86a27bb27b33c3964d032025c46f04646ea3
Ernesto Sola-Thomas <ernesto@solathomas.com>
2023-10-15 15:56:07 -0400

    Initial ialu add sequence

---
+++ src/top/verif/uvm/Sequencer/gen_ialu_add_seq.svh
@@ -0,0 +1,22 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+// TODO Initial state only generates 1 addition instruction
+class gen_ialu_add_seq extends uvm_sequence;
+    `uvm_object_utils(gen_ialu_add_seq)
+
+    function new (string name = "DEFAULT ialu add sequence");
+        super.new(name);
+    endfunction;
+
+    ialu_add_item add_item;
+
+    virtual task body();
+        `uvm_info(get_type_name(), $sformatf("Sending random addition packet"), UVM_DEFAULT)
+
+        add_item = ialu_add_item::type_id::create("Initial add ialu item");
+
+        start_item(add_item);
+        add_item.randomize()
+        finish_item(add_item);
+    endtask
+endclass
