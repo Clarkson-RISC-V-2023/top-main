dee010a5b43473f6cb0cec305911ca69065020d3
Ernesto Sola-Thomas <ernesto@solathomas.com>
2023-11-10 18:43:59 -0500

    Clean start UVM for top

--- src/top/verif/files_uvm.f
+++ src/top/verif/files_uvm.f
@@ -1,23 +1,3 @@
 -f ./files.f
 
-./src/top/verif/uvm/params/uvm_params.sv
-
-./src/top/verif/uvm/Interface/top_vif.sv
-
-./src/top/verif/uvm/Sequence/Sequence_items/instruction-type/risc-v_instruction_types.svh
-./src/top/verif/uvm/Sequence/Sequence_items/alu/ialu_items.svh
-./src/top/verif/uvm/Sequence/gen_ialu_addi_seq.svh
-
-./src/top/verif/uvm/Driver/generic_rom_instr_driver.svh
-
-./src/top/verif/uvm/Scoreboard/ialu_add_scoreboard.svh
-
-./src/top/verif/uvm/Monitor/ialu_add_monitor.svh
-
-./src/top/verif/uvm/Agent/ialu_addi_agent.svh
-
-./src/top/verif/uvm/Env/ialu_addi_env.svh
-
-./src/top/verif/uvm/Test/verify_ialu_addi.svh
-
 ./src/top/verif/uvm/tb_uvm_top.sv
--- src/top/verif/uvm/params/uvm_params.sv
+++
@@ -1,5 +0,0 @@
-package uvm_params;
-    // Size of register file, standard is 32 so 0-32
-    parameter rs_min = 0;
-    parameter rsw_max = 32;
-endpackage
